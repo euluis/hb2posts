c49a5ddf0fea941800300cdaec8ceb90e285eb3a
Luis Sergio Oliveira <euluis1@gmail.com>
2011-05-08 21:02:20 +0100

    preparations to publish in GitHub as open source project
    - licensed under EPL
    - added README as instructed by GitHub
    - __init__.py and other files are now in Unix and UTF-8 format as the other files
    - all files have EPL and copyright notice

--- hb2post.py
+++ hb2post.py
@@ -2,6 +2,17 @@
 # By Luis Sergio Oliveira a command line utility to extract blog posts from my
 # digital programming handbook.
 
+# Copyright (c) 2010-2011 Contributors - see below
+# All rights reserved.
+# The use and distribution terms for this software are covered by the
+# Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+# which can be found in the file epl-v10.html at the root of this distribution.
+# By using this software in any fashion, you are agreeing to be bound by
+# the terms of this license.
+# You must not remove this notice, or any other, from this software.
+# Contributors:
+# - Luis Sergio Oliveira (euluis)
+
 from htmled import HbFile, PostExtractor
 import sys
 import os
--- htmled_tests.py
+++ htmled_tests.py
@@ -3,6 +3,17 @@
 
 # Automated tests for htmled module.
 
+# Copyright (c) 2006-2011 Contributors - see below
+# All rights reserved.
+# The use and distribution terms for this software are covered by the
+# Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+# which can be found in the file epl-v10.html at the root of this distribution.
+# By using this software in any fashion, you are agreeing to be bound by
+# the terms of this license.
+# You must not remove this notice, or any other, from this software.
+# Contributors:
+# - Luis Sergio Oliveira (euluis)
+
 import unittest
 from datetime import date
 from htmled import *
