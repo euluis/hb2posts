702ea2de6dd0747ea39bba102a5c38daea7b6ba4
Luis Sergio Oliveira <euluis1@gmail.com>
2011-05-13 20:04:23 +0100

    starting the port to Clojure from lein generated project

---
+++ hb2posts-clj/src/hb2posts_clj/core.clj
@@ -0,0 +1,1 @@
+(ns hb2posts-clj.core)
---
+++ hb2posts-clj/test/hb2posts_clj/test/core.clj
@@ -0,0 +1,6 @@
+(ns hb2posts-clj.test.core
+  (:use [hb2posts-clj.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
