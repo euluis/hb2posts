83dcff5ba27885391c7f4c3b6bfa32669e6b9c96
Luis Sergio Oliveira <euluis1@gmail.com>
2011-05-16 09:49:47 +0100

    has 2 working tests :-)

--- hb2posts-clj/project.clj
+++ hb2posts-clj/project.clj
@@ -1,4 +1,21 @@
+;; -*- mode: Clojure; coding: utf-8 -*-
+;; Clojure file - http://clojure.org/
+;; Leinigen project file - http:// FIXME
+;; By Luis Sergio Oliveira a command line utility to extract blog posts from my
+;; digital programming handbook.
+
+;; Copyright (c) 2011 Contributors - see below
+;; All rights reserved.
+;; The use and distribution terms for this software are covered by the
+;; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;; which can be found in the file epl-v10.html at the root of this distribution.
+;; By using this software in any fashion, you are agreeing to be bound by
+;; the terms of this license.
+;; You must not remove this notice, or any other, from this software.
+;; Contributors:
+;; - Luis Sergio Oliveira (euluis)
+
 (defproject hb2posts-clj "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+  :description "Automation for my HTML and Handbook to blog author's tasks."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]])
--- hb2posts-clj/test/hb2posts_clj/test/core.clj
+++ hb2posts-clj/test/hb2posts_clj/test/core.clj
@@ -1,6 +1,26 @@
+;; -*- mode: Clojure; coding: utf-8 -*-
+;; Clojure file - http://clojure.org/
+
+;; Automated tests for hb2posts-clj project.
+
+;; Copyright (c) 2011 Contributors - see below
+;; All rights reserved.
+;; The use and distribution terms for this software are covered by the
+;; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;; which can be found in the file epl-v10.html at the root of this distribution.
+;; By using this software in any fashion, you are agreeing to be bound by
+;; the terms of this license.
+;; You must not remove this notice, or any other, from this software.
+;; Contributors:
+;; - Luis Sergio Oliveira (euluis)
+
 (ns hb2posts-clj.test.core
   (:use [hb2posts-clj.core] :reload)
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest basic-file-name-fns
+  (testing "that hb-file-name will return the correct file names"
+    (are [expected-file-name file-name]
+	 (= expected-file-name (hb-file-name file-name))
+	 "filename.html" "filename.html"
+	 "x.y" "/foo/bar/x.y")))
