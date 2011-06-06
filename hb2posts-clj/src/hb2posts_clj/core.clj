2a15cd0b5dd0c3da39907f15a6608d6244a8ebd8
Luis Sergio Oliveira <euluis1@gmail.com>
2011-06-07 00:59:18 +0100

    test cmd-line-options is green in a meaningful way
    REPL session (started with
                   $ lein swank
                   M-x slime-connect):
    ; SLIME 20100404
    user>
    nil
    user> (use 'hb2posts-clj.core)
    nil
    user> (hb2posts-clj.core/hb-file-name "ola/ola.txt")
    "ola.txt"
    user> (require 'clojure.string)
    nil
    user> (doc require)
    -------------------------
    clojure.core/require
    (...)
    nil
    user> (require '[clojure.string :as str])
    nil
    user> (doc str/split)
    -------------------------
    clojure.string/split
    ([s re] [s re limit])
      Splits string on a regular expression.  Optional argument limit is
      the maximum number of splits. Not lazy. Returns vector of the splits.
    nil
    user> (def cmd-line "--start-date 2010-01-01 --end-date 2011-01-01")
     #'user/cmd-line
    user> (str/split cmd-line #"--start-date|--end-date")
    ["" " 2010-01-01 " " 2011-01-01"]
    user> (map str/trim (remove str/blank? (str/split cmd-line #"--start-date|--end-date")))
    ("2010-01-01" "2011-01-01")
    user> (->> (str/split cmd-line #"--start-date|--end-date")
    	   (remove str/blank?)
    	   (map str/trim))
    ("2010-01-01" "2011-01-01")
    user> (in-ns 'hb2posts-clj.core)
     #<Namespace hb2posts-clj.core>
    hb2posts-clj.core> (def *cmd-line* "--start-date 2010-01-01 --end-date 2011-01-01")
     #'hb2posts-clj.core/*cmd-line*
    hb2posts-clj.core> (cmd-line-options *cmd-line*)
    ("2010-01-01" "2011-01-01")
    hb2posts-clj.core> (doc interpose)
    -------------------------
    clojure.core/interpose
    ([sep coll])
      Returns a lazy seq of the elements of coll separated by sep
    nil
    hb2posts-clj.core> (interleave {:sd :ed} (cmd-line-options *cmd-line*))
    ([:sd :ed] "2010-01-01")
    hb2posts-clj.core> (map #(list %1 %2) [:sd :ed] (cmd-line-options *cmd-line*))
    ((:sd "2010-01-01") (:ed "2011-01-01"))
    hb2posts-clj.core> (interleave [:sd :ed] (cmd-line-options *cmd-line*))
    (:sd "2010-01-01" :ed "2011-01-01")
    hb2posts-clj.core> *1
    (:sd "2010-01-01" :ed "2011-01-01")
    hb2posts-clj.core> (apply hash-map *1)
    {:sd "2010-01-01", :ed "2011-01-01"}
    hb2posts-clj.core> (def *sd-re-m* (re-matcher #"--start-date (\d\d\d\d-\d\d-\d\d)" *cmd-line*))
     #'hb2posts-clj.core/*sd-re-m*
    hb2posts-clj.core> (re-find *sd-re-m*)
    ["--start-date 2010-01-01" "2010-01-01"]
    hb2posts-clj.core> (re-groups *sd-re-m*)
    ["--start-date 2010-01-01" "2010-01-01"]
    hb2posts-clj.core> (def *sd-re-m* (re-matcher #"--start-date\s+(\d\d\d\d-\d\d-\d\d)" *cmd-line*))
     #'hb2posts-clj.core/*sd-re-m*
    hb2posts-clj.core> (re-find *sd-re-m*)
    ["--start-date 2010-01-01" "2010-01-01"]
    hb2posts-clj.core> (re-groups *sd-re-m*)
    ["--start-date 2010-01-01" "2010-01-01"]
    hb2posts-clj.core> (concat {:s "1"} {
    			   :e "2"})
    ([:s "1"] [:e "2"])
    hb2posts-clj.core> (apply hash-map *1)
    {[:s "1"] [:e "2"]}
    hb2posts-clj.core> (into {} [[:s 1] [:e 2]])
    {:s 1, :e 2}
    hb2posts-clj.core> (cmd-line-options *cmd-line*)
    {:start-date "2010-01-01", :end-date "2011-01-01"}
    hb2posts-clj.core> (in-ns 'hb2posts-clj.test.core)
     #<Namespace hb2posts-clj.test.core>
    hb2posts-clj.test.core> (hb2post-cmd-line-options)
    nil
    hb2posts-clj.test.core> (basic-file-name-fns)
    nil
    ; changed it to make it fail
    hb2posts-clj.test.core> (basic-file-name-fns)
    
    FAIL in (basic-file-name-fns) (core.clj:23)
    that hb-file-name will return the correct file names
    expected: (= "filename.html" (hb-file-name "fidlename.html"))
      actual: (not (= "filename.html" "fidlename.html"))
    nil
    ; ditto
    hb2posts-clj.test.core> (hb2post-cmd-line-options)
    
    FAIL in (hb2post-cmd-line-options) (core.clj:32)
    that cmd-line-options correctly processes start and end dates
    expected: (let [{start-date :start-date, end-date :end-date, :as options} (cmd-line-options "--start-date 2010-01-01 --end-date 2011-01-02")] (and (= "2010-01-01" start-date) (= "2011-01-01" end-date)))
      actual: false
    nil
    hb2posts-clj.test.core>

--- hb2posts-clj/src/hb2posts_clj/core.clj
+++ hb2posts-clj/src/hb2posts_clj/core.clj
@@ -15,7 +15,20 @@
 ;; - Luis Sergio Oliveira (euluis)
 
 (ns hb2posts-clj.core
-  (:import (java.io File)))
+  (:import (java.io File))
+  (:require [clojure.string :as str]))
 
 (defn hb-file-name [file-name]
   (.getName (File. file-name)))
+
+(defn cmd-line-options [cmd-line]
+  (->> (str/split cmd-line #"--start-date|--end-date")
+       (remove str/blank?)
+       (map str/trim))
+  (into {} (map (fn [[option-key option-re]]
+                  (let [option-matcher (re-matcher option-re cmd-line)]
+                    [option-key
+                     (when (re-find option-matcher)
+                       (second (re-groups option-matcher)))]))
+                [[:start-date #"--start-date\s+(\d\d\d\d-\d\d-\d\d)"]
+                 [:end-date #"--end-date\s+(\d\d\d\d-\d\d-\d\d)"]])))
