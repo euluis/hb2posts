ea1304f33f5aeed312212012eed497133bd452de
Luis Sergio Oliveira <euluis1@gmail.com>
2011-06-07 09:10:52 +0100

    failing test for not implemented default start and end dates
    REPL
    ; SLIME 20100404
    user> (import '(java.text DateFormat))
    java.text.DateFormat
    user> (DateFormat/getDateTimeInstance DateFormat/SHORT DateFormat/SHORT)
     #<SimpleDateFormat java.text.SimpleDateFormat@a9427c06>
    user> (DateFormat/getInstance)
     #<SimpleDateFormat java.text.SimpleDateFormat@a9427c06>
    user> (DateFormat/getDateInstance DateFormat/SHORT)
     #<SimpleDateFormat java.text.SimpleDateFormat@8629ad2d>
    user> (import '(java.util Date))
    java.util.Date
    user> (Date.)
     #<Date Tue Jun 07 01:20:16 WEST 2011>
    user> (DateFormat/getDateInstance DateFormat/SHORT)
     #<SimpleDateFormat java.text.SimpleDateFormat@8629ad2d>
    user> (. (DateFormat/getDateInstance DateFormat/SHORT) format (Date.))
    "6/7/11"
    user> (. (DateFormat/getDateInstance DateFormat/MEDIUM) format (Date.))
    "Jun 7, 2011"
    user> (import '(java.util Calendar))
    java.util.Calendar
    user> (Calendar/getInstance)
     #<GregorianCalendar (...)>
    user> (. (Calendar/getInstance) get Calendar/YEAR)
    2011
    user> (str (. (Calendar/getInstance) get Calendar/YEAR))
    "2011"
    user> (interleave (str (. (Calendar/getInstance) get Calendar/YEAR)) (str (. (Calendar/getInstance) get Calendar/MONTH)) (str (. (Calendar/getInstance) get Calendar/DAY_OF_MONTH)) ["-" "-"])
    (\2 \5 \7 "-")
    user> (interleave [(str (. (Calendar/getInstance) get Calendar/YEAR)) (str (. (Calendar/getInstance) get Calendar/MONTH)) (str (. (Calendar/getInstance) get Calendar/DAY_OF_MONTH))] ["-" "-"])
    ("2011" "-" "5" "-")
    user> (interleave [(str (. (Calendar/getInstance) get Calendar/YEAR)) (str (. (Calendar/getInstance) get Calendar/MONTH)) (str (. (Calendar/getInstance) get Calendar/DAY_OF_MONTH))] ["-" "-" "-"])
    ("2011" "-" "5" "-" "7" "-")
    user> (str (. (Calendar/getInstance) get Calendar/MONTH))
    "5"
    user> (= Calendar/JUNE (. (Calendar/getInstance) get Calendar/MONTH))
    true
    user> (interleave [(str (. (Calendar/getInstance) get Calendar/YEAR)) (str (+ 1 (. (Calendar/getInstance) get Calendar/MONTH))) (str (. (Calendar/getInstance) get Calendar/DAY_OF_MONTH))] ["-" "-" ""])
    ("2011" "-" "6" "-" "7" "")
    ; now I implemented format-iso fn
    user> (hb2posts-clj.test.core/format-iso (java.util.Date.))
    "2011-06-07"
    user> (in-ns 'hb2posts-clj.test.core)
     #<Namespace hb2posts-clj.test.core>
    hb2posts-clj.test.core> (hb2post-cmd-line-options)
    
    FAIL in (hb2post-cmd-line-options) (core.clj:42)
    that cmd-line-options correctly processes start and end dates
    expected: (let [{start-date :start-date, end-date :end-date, :as options} (cmd-line-options "")] (and (= (format-iso (Date.)) start-date) (= (format-iso (Date.)) end-date)))
      actual: false
    nil
    hb2posts-clj.test.core>

--- hb2posts-clj/test/hb2posts_clj/test/core.clj
+++ hb2posts-clj/test/hb2posts_clj/test/core.clj
@@ -16,7 +16,8 @@
 
 (ns hb2posts-clj.test.core
   (:use [hb2posts-clj.core] :reload)
-  (:use [clojure.test]))
+  (:use [clojure.test])
+  (:import (java.util Calendar Date)))
 
 (deftest basic-file-name-fns
   (testing "that hb-file-name will return the correct file names"
@@ -27,6 +28,17 @@
 	 "name.ext" "../foo/bar/name.ext"
 	 "name" "/absolute/path/name")))
 
+(defn format-2d [n]
+  (if (>= n 10) (str n) (str "0" n)))
+
+;;; WTF, Java doesn't have this!?!
+(defn format-iso [d]
+  (let [calendar (Calendar/getInstance)]
+    (str (. calendar get Calendar/YEAR) "-"
+         ;; WTF, stupid and wrong implementation!
+         (format-2d (+ 1 (. calendar get Calendar/MONTH))) "-"
+         (format-2d (. calendar get Calendar/DAY_OF_MONTH)))))
+
 (deftest hb2post-cmd-line-options
   (testing "that cmd-line-options correctly processes start and end dates"
     (are [expected-start-date expected-end-date cmd-line]
@@ -34,4 +46,8 @@
 	       (cmd-line-options cmd-line)]
 	   (and (= expected-start-date start-date)
 		(= expected-end-date end-date)))
-	 "2010-01-01" "2011-01-01" "--start-date 2010-01-01 --end-date 2011-01-01")))
+	 "2010-01-01" "2011-01-01" "--start-date 2010-01-01 --end-date 2011-01-01"
+         ;; FIXME: difficult to understand what went wrong when fails
+         ;; FIXME: fragile around midnight
+         ;; TODO: in cmd-line-options default start date and end date are today's date
+         (format-iso (Date.)) (format-iso (Date.)) "")))
